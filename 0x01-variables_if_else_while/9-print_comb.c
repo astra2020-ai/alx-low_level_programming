b34d94725419082a2f0760250a479ec0c6b23376
astra2020-ai <opioambrose001@gmail.com>
2022-07-01 18:35:40 +0300

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,6 +8,7 @@
 int main(void)
 {
 	int num;
+
 	for (num = 0; num <= 9; num++)
 	{
 		putchar((num % 10) + '0');
@@ -17,5 +18,7 @@ int main(void)
 		putchar(',');
 		putchar(' ');
 	}
+
 	putchar('\n');
+
 	return (0);
